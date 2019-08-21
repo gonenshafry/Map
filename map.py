9ef427344e215c8716746f57fa641c0e7d5d3a31
gonenshafry <gonenshafry@users.noreply.github.com>
2019-08-21 12:01:16 +0300

    Update map.py

--- map.py
+++ map.py
@@ -1,40 +1,40 @@
-import folium;
-import pandas;
+import folium
+import pandas
 
-data = pandas.read_csv("Volcanoes.txt");
-lon = list(data["LON"]);
-lat = list(data["LAT"]);
-elev = list(data["ELEV"]);
-name = list(data["NAME"]);
+data = pandas.read_csv("Volcanoes.txt")
+lon = list(data["LON"])
+lat = list(data["LAT"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
 
 def color_producer(elevation):
  if elevation < 1000:
-  return 'green';
+  return 'green'
  elif 1000 <= elevation < 3000:
-  return 'orange';
+  return 'orange'
  else:
-  return 'red';
+  return 'red'
 
 map = folium.Map(location=[38.58,-99.09],
-  zoom_start=4, width=900, height=700);
+  zoom_start=4, width=900, height=700)
 
-fgv = folium.FeatureGroup(name="Volcanoes");
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt,ln,el,nm in zip(lat,lon,elev,name):
  fgv.add_child(folium.CircleMarker(location=[lt,ln],
   radius=6, color='rey', fill_opacity=0.7,
   popup=nm + " Elevation: " + str(el) + "m",
-  fill_color=color_producer(el) ));
+  fill_color=color_producer(el) ))
 
-fgp = folium.FeatureGroup(name="Population");
+fgp = folium.FeatureGroup(name="Population")
 
 fgp.add_child(folium.GeoJson(data=(open('world.json','r',encoding='utf-8-sig').read()),
   style_function=lambda x: {'fillColor':'green'
     if x['properties']['POP2005'] < 10000000
-    else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}));
+    else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fgv);
-map.add_child(fgp);
-map.add_child(folium.LayerControl());
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
-map.save("Map.html");
+map.save("Map.html")
